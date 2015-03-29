19a41d51a3b03ac094fbc6db136ef1000240c6de
Tina Chen <tl2chen@uwaterloo.ca>
2015-03-29 04:47:21 -0400

    Fixed updating weather data

--- web/js/main.js
+++ web/js/main.js
@@ -1,30 +1,45 @@
 var table_row_template = $('.table-row-template').remove(),
-    table              = $('.weather-table');
+    current_row_template = $('.current-row-template').remove();
 
 reloadWeather();
 var myVar;
 function reloadWeather() {
 
- myVar= setTimeout(function() {
+ myVar = setTimeout(function() {
 
     API.fetch_weather(function (response) {
 
+      $('.current-row-template').remove();
       $('.table-row-template').remove();
+
       var data = JSON.parse(response);
+
+      var condition = data.query.results.channel.item.condition;
+      var atmosphere = data.query.results.channel.atmosphere;
+          
+            var new_row = current_row_template.clone();
+
+            new_row.find('#todayDate').text(condition['date']);
+            new_row.find('#todayTemp').text(Math.round((condition['temp']-32) * 5/9));
+            new_row.find('#todayLow').text(atmosphere['humidity']);
+            new_row.find('#todayPOP').text(condition['text']);
+          
+            $('#currentDay').append(new_row);
 
       var forecast = data.query.results.channel.item.forecast;
 
       for(var index = 0; index < forecast.length; ++index) {
             var day = forecast[index];
           
-            var new_row = $('.table-row-template').clone();
+            var new_row = table_row_template.clone();
 
-            new_row.find('#foreDate').text(day['day']);
-            new_row.find('#foreHigh').text(day['high']);
-            new_row.find('#foreLow').text(day['low']);
+            new_row.find('#foreDate').text(day['day'] + ' ' + day['date']);
+            new_row.find('#foreHigh').text(Math.round((day['high']-32) * 5/9));
+            new_row.find('#foreLow').text(Math.round((day['low']-32) * 5/9));
             new_row.find('#foreDetails').text(day['text']);
           
             $('#forecast').append(new_row);
+            
         
 
       };
@@ -38,5 +53,3 @@ function reloadWeather() {
 
 
 } 
-
-2
--- web/zerver/API.js
+++ web/zerver/API.js
@@ -6,7 +6,7 @@ function get_weather( callback) {
 
   var options = {
     hostname  : 'query.yahooapis.com',
-    path      : '/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+    path      : '/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22toronto%2C%20ontario%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
     port      : 443,
     method    : 'GET',
    
