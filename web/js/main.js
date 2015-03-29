66948308ed2d05d1492ff1ebce2b169683aaa79e
Tina Chen <tl2chen@uwaterloo.ca>
2015-03-29 02:55:55 -0400

    switch API to Yahoo

--- web/js/main.js
+++ web/js/main.js
@@ -1,26 +1,41 @@
 var table_row_template = $('.table-row-template').remove(),
     table              = $('.weather-table');
 
+reloadWeather();
+var myVar;
+function reloadWeather() {
 
-API.fetch_weather('Calgary', function (response) {
+ myVar= setTimeout(function() {
+
+    API.fetch_weather(function (response) {
+
+      $('.table-row-template').remove();
+      var data = JSON.parse(response);
+
+      var forecast = data.query.results.channel.item.forecast;
+
+      for(var index = 0; index < forecast.length; ++index) {
+            var day = forecast[index];
+          
+            var new_row = table_row_template.clone();
+
+            new_row.find('#day').text(day['day']);
+            new_row.find('#high').text(day['high']);
+            new_row.find('#low').text(day['low']);
+
+            table.append(new_row);
+        
+
+      };
+      console.log("ran");
+    
+      reloadWeather();
+      
+
+    });
+  }, 2000);
 
 
-  var data = JSON.parse(response);
+} 
 
-  $('#main-text').text(data[0]['condition']);
-
-
-  data.forEach(function (day) {
-
-    var new_row = table_row_template.clone();
-
-    new_row.find('#day').text(day['day_of_week']);
-    new_row.find('#high').text(day['high']);
-    new_row.find('#low').text(day['low']);
-
-    table.append(new_row);
-  });
-
-
-
-});
+2
--- web/zerver/API.js
+++ web/zerver/API.js
@@ -1,17 +1,15 @@
 var http = require('https');
 
 
-function get_weather(city, callback) {
-  var city = city.replace(' ', '+');
+function get_weather( callback) {
+ // var city = city.replace(' ', '+');
 
   var options = {
-    hostname  : 'george-vustrey-weather.p.mashape.com',
-    path      : '/api.php?location=' + city,
+    hostname  : 'query.yahooapis.com',
+    path      : '/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
     port      : 443,
     method    : 'GET',
-    headers   : {
-      'X-Mashape-Key' : 'U72EQEwYHcmshta7CdQLvkO2ADq6p1PQDWNjsnu5V9KLbex6ED'
-    }
+   
   }
 
   var req = http.request(options, function (res) {
@@ -40,6 +38,6 @@ function get_weather(city, callback) {
 
 }
 
-exports.fetch_weather = function (city, cb) {
-  get_weather(city, cb);
+exports.fetch_weather = function ( cb) {
+  get_weather(cb);
 }
